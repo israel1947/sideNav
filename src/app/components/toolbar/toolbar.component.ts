d31b52cc5f45c85df492e5faf8039d6e35527c7f
israel_1947 <israeldavidsdvsf@gmail.com>
2023-08-07 13:50:29 +0200

    The time is saw in the toolbar componet

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements AfterViewInit {
+
+  
+  constructor() { }
+
+  @ViewChild('hr') hr!:ElementRef;
+
+  ngAfterViewInit(): void {
+    setInterval(() => {
+      const date:Date = new Date;
+      this.hr.nativeElement.innerText = date.toLocaleTimeString('es-EU',{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit'
+      })
+    }, 1000);
+  }
+
+
+  tiempo = new Date();
+  diaSemana = this.tiempo.toDateString();
 
 }
